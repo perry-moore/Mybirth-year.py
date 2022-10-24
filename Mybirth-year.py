90e66a884562ff191e40f8813bedfb44c5093533
Kelvin Perry Boamah Mensah <99218715+perry-moore@users.noreply.github.com>
2022-10-24 08:30:03 -0700

    Add files via upload

--- Mybirth-year.py
+++ Mybirth-year.py
@@ -1,16 +1,8 @@
-name = 'Abigail'
-if name == 'Mary':
-    print('Hello Mary')
-password = 'swordfish'
-if password == 'swordfish':
-    print('Access granted.')
-else:
-    print('Wrong password')
 print("What is your birth_year?") 
 # Asking for your birth_year
 myBirth_year = int(input("Please enter the year you were born"))
 Currentyear = int(input("Kindly provide the current year that you are in"))
-myAge= Currentyear- myBirth_year
+myAge = Currentyear- myBirth_year
 if myAge == 19 :
     print("Hurry, you are " + str(myAge) + " years old")
 else:
