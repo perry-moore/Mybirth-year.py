2bed1e182373220539098a5fff7f17bc3d7e15a9
Kelvin Perry Boamah Mensah <99218715+perry-moore@users.noreply.github.com>
2022-11-04 07:50:36 -0700

    Update Mybirth-year.py

--- Mybirth-year.py
+++ Mybirth-year.py
@@ -1,8 +1,8 @@
 print("What is your birth_year?") 
 # Asking for your birth_year
-myBirth_year = int(input("Please enter the year you were born"))
-Currentyear = int(input("Kindly provide the current year that you are in"))
-myAge = Currentyear- myBirth_year
+myBirth_year = int(input("Please enter the year you were born:"))
+Current_year = int(input("Kindly provide the current year that you are in:"))
+myAge = Current_year- myBirth_year
 if myAge == 19 :
     print("Hurry, you are " + str(myAge) + " years old")
 else:
